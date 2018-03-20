1bba34c5ecae1b25553c84677e83aa07320d3ca9
Clemens Anzmann <clemensa@colbenson.com>
2018-03-20 12:48:35 +0000

    add simple map

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+var width = 1000, height = 425;
+
+var path = d3.geoPath()
+ .projection(d3.geoEquirectangular());
+
+var svg = d3
+  .select('#my-map')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+d3.json(
+  './globe.geo.json', function(json) {
+  countriesGroup = svg
+   .append('g')
+   .attr('id', 'map');
+
+  countriesGroup
+   .append('rect')
+   .attr('x', 0)
+   .attr('y', 0)
+   .attr('width', width)
+   .attr('height', height);
+
+  countries = countriesGroup
+   .selectAll('path')
+   .data(json.features)
+   .enter()
+   .append('path')
+   .attr('d', path)
+   .attr('id', function(d, i) {
+      return 'country' + d.properties.iso_a3;
+   })
+   .attr('class', 'country');
+});
