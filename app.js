9ab63d4eac49c15d7d6f727cc99642439f5d272d
Clemens Anzmann <clemensa@colbenson.com>
2018-03-31 18:29:37 +0100

    add first arc

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var width = 1000, height = 425;
+var width = 1000,
+  height = 425;
 
 var path = d3.geoPath()
- .projection(d3.geoEquirectangular());
+  .projection(d3.geoEquirectangular());
+
+var lineData = [{
+  type: 'LineString',
+  coordinates: [
+    [-0.461941, 51.4706],
+    [151.177002, -33.94609833]
+  ]
+}];
 
 var svg = d3
   .select('#my-map')
@@ -10,26 +19,34 @@ var svg = d3
   .attr('height', height);
 
 d3.json(
-  './globe.geo.json', function(json) {
-  countriesGroup = svg
-   .append('g')
-   .attr('id', 'map');
+  './globe.geo.json',
+  function(json) {
+    countriesGroup = svg
+      .append('g')
+      .attr('id', 'map');
 
-  countriesGroup
-   .append('rect')
-   .attr('x', 0)
-   .attr('y', 0)
-   .attr('width', width)
-   .attr('height', height);
+    countriesGroup
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', width)
+      .attr('height', height);
 
-  countries = countriesGroup
-   .selectAll('path')
-   .data(json.features)
-   .enter()
-   .append('path')
-   .attr('d', path)
-   .attr('id', function(d, i) {
-      return 'country' + d.properties.iso_a3;
-   })
-   .attr('class', 'country');
-});
+    countries = countriesGroup
+      .selectAll('path')
+      .data(json.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .attr('id', function(d) {
+        return 'country' + d.properties.iso_a3;
+      })
+      .attr('class', 'country');
+
+    var pathArcs = svg.selectAll('.flightarc')
+      .data(lineData)
+      .enter()
+      .append('path')
+      .attr('class', 'flightarc')
+      .attr('d', path);
+  });
